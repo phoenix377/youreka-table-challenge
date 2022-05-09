902d55b8259746a22bfdecc512f3d8b123602c57
phoenix377 <phoenix.number377@gmail.com>
2022-05-09 17:35:26 +0430

    :bento: colors added

---
+++ src/styles/EColor.ts
@@ -0,0 +1,10 @@
+export enum EColor {
+  Primary = "#04aa6d",
+  PrimaryDark = "#048153",
+
+  Gray = "#ddd",
+  Neutral = "#f2f2f2",
+  White = "#fff",
+
+  Black = "#000",
+}
---
+++ src/styles/EShadow.ts
@@ -0,0 +1,3 @@
+export enum EShadow {
+  Light = "0 0.25rem 0.5rem 0 rgba(0, 0, 0, 0.2), 0 0.375rem 1.25rem 0 rgba(0, 0, 0, 0.19)",
+}
